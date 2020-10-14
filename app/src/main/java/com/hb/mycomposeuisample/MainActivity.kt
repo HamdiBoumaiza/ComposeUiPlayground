e04b7590a0475b060da92f674341d97c3166b063
Hamdi Boumaiza <HamdiBoumaiza1@gmail.com>
2020-10-14 22:54:03 +0200

    update gradle

--- app/src/main/java/com/hb/mycomposeuisample/MainActivity.kt
+++ app/src/main/java/com/hb/mycomposeuisample/MainActivity.kt
@@ -1,99 +1,30 @@
 package com.hb.mycomposeuisample
 
 import android.os.Bundle
-import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.Composable
-import androidx.ui.core.Text
-import androidx.ui.core.dp
-import androidx.ui.core.setContent
-import androidx.ui.foundation.VerticalScroller
-import androidx.ui.graphics.Color
-import androidx.ui.layout.*
-import androidx.ui.material.*
+import androidx.compose.foundation.Text
+import androidx.compose.foundation.layout.Stack
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Button
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.setContent
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent { displayButtons() }
+        setContentViewFromCompose()
     }
 
     @Composable
-    fun hello(name: String) = MaterialTheme {
-        FlexColumn {
-            inflexible {
-                // Item height will be equal content height
-                TopAppBar<MenuItem>( // App Bar with title
-                    title = { Text("Jetpack Compose Sample") }
-                )
-            }
-            expanded(1F) {
-                // occupy whole empty space in the Column
-                Center {
-                    // Center content
-                    Text("Hello $name!") // Text label
-                }
-            }
-        }
-    }
-
-    @Composable
-    private fun displayButtons() {
-        Column {
-            Button(
-                text = "This is Button 1",
-                onClick = {
-                    //the click listeners
-                },
-                style = ContainedButtonStyle()
-            )
-            HeightSpacer(32.dp)
-            Button(
-                text = "This is Button 2",
-                onClick = {
-                    //the click listeners
-                },
-                style = OutlinedButtonStyle()
-            )
-        }
-    }
-
-    @Composable
-    private fun createListView() {
-        MaterialTheme {
-            VerticalScroller {
-                Column {
-                    (0..10).forEachIndexed { index, _ ->
-                        createListItem(index)
-                        Divider(color = Color.Blue, height = 1.dp)
-                    }
-                }
-            }
-
-        }
-    }
-
-    @Composable
-    private fun createListItem(itemIndex: Int) {
-        Padding(left = 8.dp, right = 8.dp, top = 8.dp, bottom = 8.dp) {
-            FlexRow(crossAxisAlignment = CrossAxisAlignment.Center) {
-                expanded(1.0f) {
-                    Text("Item $itemIndex")
-                }
-                inflexible {
-                    Button(
-                        "Button $itemIndex",
-                        style = ContainedButtonStyle(),
-                        onClick = {
-                            Toast.makeText(
-                                this@MainActivity,
-                                "Item name $itemIndex",
-                                Toast.LENGTH_SHORT
-                            ).show()
-                        })
-
+    private fun setContentViewFromCompose() {
+        setContent {
+            Stack(modifier = Modifier.fillMaxSize()) {
+                Button(onClick = { Toast.makeText(this@MainActivity, "0", Toast.LENGTH_LONG).show() }, modifier = Modifier.gravity(Alignment.Center)) {
+                    Text(text = "tvtv")
                 }
             }
         }
